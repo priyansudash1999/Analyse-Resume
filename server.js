bad9aed04a10a04f0cab9b1417c729811f604259
1999priyansu <1999priyansudash@gmail.com>
2026-03-07 20:58:20 +0530

    feat:Setup Project

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv";
+import app from "./src/app.js"
+import connectDb from "./src/config/db.js";
+
+dotenv.config();
+
+connectDb()
+
+const mongodb_uri = process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000
+
+console.log(mongodb_uri);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
